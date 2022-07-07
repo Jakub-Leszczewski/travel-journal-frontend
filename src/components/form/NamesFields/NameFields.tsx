788a11accd45295199a7bad73dd1bc5a05e840ad
JakubL <jakubxleszczewski@gmail.com>
2022-07-07 23:22:46 +0200

    create name fields

---
+++ src/components/form/NamesFields/NameFields.tsx
@@ -0,0 +1,33 @@
+import React, { ChangeEvent } from 'react';
+import { ShortTextInput } from '../../common/ShortTextInput/ShortTextInput';
+import './NameFields.css';
+
+interface Props {
+  form: {
+    firstName: string;
+    lastName: string;
+  }
+  changeFormHandler: (e: ChangeEvent<HTMLInputElement>) => void
+}
+
+export function NameFields({ form, changeFormHandler }: Props) {
+  return (
+    <div className="NameFields">
+      <ShortTextInput
+        value={form.firstName}
+        name="firstName"
+        onChange={changeFormHandler}
+        placeholder="Imie"
+        required
+      />
+
+      <ShortTextInput
+        value={form.lastName}
+        name="lastName"
+        onChange={changeFormHandler}
+        placeholder="Nazwisko"
+        required
+      />
+    </div>
+  );
+}
