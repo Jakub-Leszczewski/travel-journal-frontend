58abacf26080547b26625b1f92124dde33c1f412
JakubL <jakubxleszczewski@gmail.com>
2022-07-10 22:12:56 +0200

    create class to generate form data from form object

--- src/utils/create-form-data.ts
+++ src/utils/create-form-data.ts
@@ -1,10 +1,27 @@
-export function createFormData(obj: any) {
-  const objEntries = Object.entries(obj);
-  const formData = new FormData();
-  for (const objEntry of objEntries) {
-    if (objEntry[1] instanceof File) formData.append(objEntry[0], objEntry[1]);
-    if (objEntry[1]) formData.append(objEntry[0], String(objEntry[1]));
+export class CreateFormData {
+  static createFormData(obj: any) {
+    const objEntries = Object.entries(obj);
+    const formData = new FormData();
+
+    for (const objEntry of objEntries) {
+      if (objEntry[1] instanceof File) formData.append(objEntry[0], objEntry[1]);
+      else formData.append(objEntry[0], String(objEntry[1]));
+    }
+
+    return formData;
   }
 
-  return formData;
+  static createFormDataRemoveEmpty(obj: any) {
+    const objEntries = Object.entries(obj);
+    const formData = new FormData();
+
+    for (const objEntry of objEntries) {
+      if (objEntry[1] instanceof File) formData.append(objEntry[0], objEntry[1]);
+      if (objEntry[1] !== '' && objEntry[1] !== undefined) {
+        formData.append(objEntry[0], String(objEntry[1]));
+      }
+    }
+
+    return formData;
+  }
 }
